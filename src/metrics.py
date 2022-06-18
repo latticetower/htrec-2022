2e6d9f04e1d089afd7740bdbd953a74cfb780a3e
latticetower <merlettaia@gmail.com>
2022-06-19 01:58:52 +0200

    add CERR computation

---
+++ src/metrics.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pywer
+
+def compute_metrics(human_translations, system_translations, candidate_translations):
+  def compute_cer_values(avalues, bvalues):
+    # train_df["R1_CER"] = train_df.apply(lambda row: pywer.cer([row.HUMAN_TRANSCRIPTION], [R1]), axis=1)
+    return np.asarray([pywer.cer([a], [b]) for a, b in zip(avalues, bvalues)])
+    
+  cer_values = compute_cer_values(human_translations, candidate_translations)
+  print(f"Candidate CER: {cer_values.mean()}")
+  # Computing the character error *reduction* rate (CERR)
+  cer_ht_st = compute_cer_values(human_translations, system_translations)
+  print(f"Candidate CERR: {(cer_values - cer_ht_st).mean()}")
